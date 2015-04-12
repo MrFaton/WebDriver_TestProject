539a8a7e226ad4f7c98342073279173d3c2eec77
Mr_Faton <firefly90@inbox.ru>
2015-04-12 21:09:55 +0300

    update from home

---
+++ src/Test/Test1.java
@@ -0,0 +1,26 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ * Created by Mr_Faton on 02.04.2015.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        WebDriver driver = new HtmlUnitDriver(true);
+//        WebDriver driver = new FirefoxDriver();
+        driver.get("http://prospero.ru/");
+//
+        System.out.println(driver.getTitle());
+//        WebElement query = driver.findElement(By.name("login"));
+//        query.sendKeys("Mr_Faton");
+//        query = driver.findElement(By.name("pass"));
+//        query.sendKeys("123");
+//        query.submit();
+////        WebElement query = driver.findElement(By.className("authorized-button"));
+////        query.click();
+//
+//        System.out.println(driver.getTitle());
+    }
+}
